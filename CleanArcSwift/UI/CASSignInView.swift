6f6747cd88e9cae8e458bce21fef98408f6b93fe
Han Sena <abisena@outlook.com>
2024-05-24 16:58:58 +0700

    Call SignIn view on ContentView

--- CleanArcSwift/UI/CASSignInView.swift
+++ CleanArcSwift/UI/CASSignInView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CASSignInView: View {
+    @Environment(\.dismiss) private var dismiss
     
     @State var email: String = ""
     @State var password: String = ""
@@ -15,124 +16,144 @@ struct CASSignInView: View {
     @State var remember: Bool = false
     
     var body: some View {
-        VStack{
-            //MARK: - Header Sign In
-            GeometryReader { proxy -> AnyView in
-                let height = proxy.frame(in: .global).height
-                AnyView(
-                    ZStack {
-                        UnevenRoundedRectangle(cornerRadii: .init(
-                            bottomLeading: 60,
-                            bottomTrailing: 60
-                        ))
-                        .fill(.black)
-                        .offset(y: -height / 2)
-                        .frame(height: 350)
-                    }
-                )
-            }
-            .frame(maxHeight: getRect().height / 1.5)
-            .overlay {
-                VStack {
-                    Image("Logo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70, height: 70)
-                    Text("Welcome Back!")
-                        .foregroundStyle(.white)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .topTrailing) {
+            VStack{
+                //MARK: - Header Sign In
+                GeometryReader { proxy -> AnyView in
+                    let height = proxy.frame(in: .global).height
+                    AnyView(
+                        ZStack {
+                            UnevenRoundedRectangle(cornerRadii: .init(
+                                bottomLeading: 60,
+                                bottomTrailing: 60
+                            ))
+                            .fill(.black)
+                            .offset(y: -height / 2)
+                            .frame(height: 350)
+                        }
+                    )
                 }
-                .padding(.bottom, 250)
-            }
-            
-            //MARK: - Sign In Form
-            ZStack {
-                VStack(spacing: 20) {
-                    HStack {
-                        Text("Sign In")
-                            .font(.largeTitle)
-                            .bold()
-                        Spacer()
+                .frame(maxHeight: getRect().height / 1.5)
+                .overlay {
+                    VStack {
+                        Image(systemName: "apple.meditate")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 70, height: 70)
+                            .foregroundStyle(.white)
+                        Text("Welcome Back!")
+                            .foregroundStyle(.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     }
-                    Group {
-                        TextField("Username", text: $email)
+                    .padding(.bottom, 250)
+                }
+                
+                //MARK: - Sign In Form
+                ZStack {
+                    VStack(spacing: 20) {
                         HStack {
-                            Group {
-                                if !showPassword {
-                                    SecureField("Password", text: $password)
-                                } else {
-                                    TextField("Password", text: $password)
+                            Text("Sign In")
+                                .font(.largeTitle)
+                                .bold()
+                            Spacer()
+                        }
+                        Group {
+                            TextField("Username", text: $email)
+                            HStack {
+                                Group {
+                                    if !showPassword {
+                                        SecureField("Password", text: $password)
+                                    } else {
+                                        TextField("Password", text: $password)
+                                    }
                                 }
+                                
+                                Button(action: {showPassword.toggle()}, label: {
+                                    Image(systemName: showPassword ? "eye.slash" : "eye")
+                                        .foregroundStyle(.black)
+                                })
                             }
+                        }
+                        .padding()
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        
+                        
+                        HStack {
+                            Toggle(isOn: $remember) {
+                               Text("Remember me")
+                                    .font(.footnote)
+                            }
+                            .toggleStyle(iOSCheckboxToggleStyle())
+                            Spacer()
+                            Text("Forgot password")
+                                .font(.footnote)
+                                .bold()
+                                .foregroundStyle(.gray)
+                        }
+                        
+                        //MARK: - Auth Methods
+                        Button(action: {}, label: {
+                            Text("Log In")
+                                .bold()
+                        })
+                        .frame(width: 350, height: 50)
+                        .background(.black)
+                        .foregroundStyle(.white)
+                        .cornerRadius(10)
+                        
+                        LabelledDivider(label: "or")
+                        
+                        Group {
+                            Button(action: {}, label: {
+                                HStack {
+                                    Image(.google50)
+                                    Text("Continue with Google")
+                                }
+                            })
                             
-                            Button(action: {showPassword.toggle()}, label: {
-                                Image(systemName: showPassword ? "eye.slash" : "eye")
-                                    .foregroundStyle(.black)
+                            Button(action: {}, label: {
+                                HStack {
+                                    Image(.apple50)
+                                    Text("Continue with Apple")
+                                }
+                            })
+                            Button(action: {}, label: {
+                                HStack {
+                                    Image(.facebook50)
+                                    Text("Continue with Facebook")
+                                }
                             })
                         }
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.05))
-                    .cornerRadius(10)
-                    
-                    
-                    HStack {
-                        Toggle(isOn: $remember) {
-                           Text("Remember me")
-                                .font(.footnote)
-                        }
-                        .toggleStyle(iOSCheckboxToggleStyle())
-                        Spacer()
-                        Text("Forgot password")
-                            .font(.footnote)
-                            .bold()
-                            .foregroundStyle(.gray)
-                    }
-                    
-                    //MARK: - Auth Methods
-                    Button(action: {}, label: {
-                        Text("Log In")
-                            .bold()
-                    })
-                    .frame(width: 350, height: 50)
-                    .background(.black)
-                    .foregroundStyle(.white)
-                    .cornerRadius(10)
-                    
-                    LabelledDivider(label: "or")
-                    
-                    Group {
-                        Button(action: {}, label: {
-                            Text("Continue with Google")
-                        })
-                        
-                        Button(action: {}, label: {
-                            Text("Continue with Apple")
-                        })
-                        Button(action: {}, label: {
-                            Text("Continue with Facebook")
-                        })
-                    }
-                    .foregroundStyle(.black)
-                    .font(.footnote)
-                    .frame(width: 350, height: 50)
-                    .background(
-                        RoundedRectangle(
-                            cornerRadius: 10,
-                            style: .continuous
-                        )
-                        .stroke(.gray, lineWidth: 1)
+                        .foregroundStyle(.black)
+                        .font(.footnote)
+                        .frame(width: 350, height: 50)
+                        .background(
+                            RoundedRectangle(
+                                cornerRadius: 10,
+                                style: .continuous
+                            )
+                            .stroke(.gray, lineWidth: 1)
 
-                    )
-                    
-                    
-                }
-                .frame(width: 350, height: 50)
-                .padding(.top, 50)
-                .frame(width: getRect().width)
-            }
-            .frame(maxHeight: .infinity, alignment: .top)
+                        )
                         
+                        
+                    }
+                    .frame(width: 350, height: 50)
+                    .padding(.top, 50)
+                    .frame(width: getRect().width)
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+                            
+            }
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "xmark.app")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                    .padding()
+            }
         }
     }
 }
--- CleanArcSwift/UI/SupportingFiles/ContentView.swift
+++ CleanArcSwift/UI/SupportingFiles/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showLogin: Bool = false
+    
     var body: some View {
         NavigationStack {
             Group {
                 CASProductView()
             }
             .navigationTitle("Products")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "line.3.horizontal.decrease.circle")
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        showLogin = true
+                    } label: {
+                        Image(systemName: "gear")
+                    }
+                    .tint(.black)
+                }
+            }
+            .fullScreenCover(isPresented: $showLogin) {
+                CASSignInView()
+            }
         }
     }
 }
