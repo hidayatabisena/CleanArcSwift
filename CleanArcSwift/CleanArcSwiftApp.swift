348945f28700863dc910fd6a51609b91789bd529
Han Sena <abisena@outlook.com>
2024-05-24 11:20:53 +0700

    Initial Commit

---
+++ CleanArcSwift/CleanArcSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArcSwiftApp.swift
+//  CleanArcSwift
+//
+//  Created by Hidayat Abisena on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CleanArcSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
